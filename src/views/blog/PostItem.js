605ee40b08326574c5ce6ac488ece7dae686a359
PriyaChilaka <priyakolukuluri@gmail.com>
2021-12-08 22:21:19 +0100

    blogg

--- src/views/blog/PostItem.js
+++ src/views/blog/PostItem.js
@@ -1,32 +1,43 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "../../style/PostItem.css";
+//import ContactPage from "../contact/ContactPage";
+//import "../../style/PostItem.css";
 
 const PostItem = ({ post }) => {
   console.log(post.acf);
   
   return (
-    <section className="blog">
-      
+  
+    <div className="flex flex-row p-8 h-full overflow-y-auto ">
+               
+    <section className="flex justify-center p-7 mt-2 bg-white rounded w-max">
+       
      
-      <div className="container grid grid-cols-1  gap-2 mx-auto bg-gradient-to-r from-yellow-400 via-red-500 to-pink-500 ... ">
-        <img className="blog-image" src={post.acf.image.url} alt={post.acf.image.alt} />
+        <div className="container grid grid-cols-1  gap-2 mx-auto bg-gradient-to-r from-yellow-400 via-red-500 to-green-500 ... ">
+          
+      <br />
+        <img className=" m-auto max-w-full object-cover" src={post.acf.image.url} alt={post.acf.image.alt} />
         <div>
-           <h1 className=" font-bold textalign-center rounded-xl p-8">{post.acf.title}</h1>
-          <p className="bg-gray open:bg-gray-100">{post.acf.description}</p>
+           <h2 className="  text-2xl ... font-sans...  my-1 font-bold flex justify-center rounded-xl p-8">{post.acf.title}</h2>
+          <p className="bg-gray mt-2 open:bg-gray-100">{post.acf.description}</p>
       <p>{post.date}</p>
-      
+      <div>
           <Link to={`/post/${post.id}`}>
-          <button className="bg-blue-500 hover:bg-blue-700 text-black font-bold py-2 px-4 rounded">
+          <button className="bg-blue-500  ml-10 mt-5 p-3 hover:bg-blue-700 flex justify-center text-black font-bold py-2 px-2 rounded-lg">
   Read more
 </button>      
       </Link>
+      </div>
         </div>
       </div>
-     
-     
+      
     </section>
+     
+ </div>
+     
+     
   );
+  
 };
 
 export default PostItem;
